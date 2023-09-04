b0ec35dd06d92f39ec2dfa8ea9008846ade42b36
JONAAVILA <jona_1969@hotmail.com>
2023-09-03 22:52:32 -0300

    'refresh web server'

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,30 +3,21 @@ const characters = require('./utils/data');
 
 const PORT = 3001;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
+    const { url } = req;
     
-    const { url } = req
-    
-    // if (req.url.startsWith("/rickandmorty/character/")) {
-    //     const id = req.url.substring(req.url.lastIndexOf("/") + 1);
-    //     const characterId = parseInt(id);
-
-    //     const character = data.characters.find(char => char.id === characterId);
-
-    if (url.includes("/rickandmorty/character")) {
-        // const id = url.split("/").pop();
+    if(url.includes('/rickandmorty/character')){
         const id = +req.url.split('/').at(-1)[0];
-        const character = characters.find(char => char.id === id);
-        res.writeHead(200, {"Content-Type":"application/json"});
-        if(character) {
+        const character = characters.find(char => char.id === id );
+        res.writeHead(200, {"Content-Type":"application/json"})
+        if(character){
             return res.end(JSON.stringify(character));
         } else {
             return res.end(JSON.stringify({"error":"Character not found"}));
         }
-    }   
+    }
     res.writeHead(404, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify({"error":"not found"}))
 });
-
-server.listen(PORT, () => {console.log(`server levantado en el puerto ${PORT}`)});
+server.listen(PORT, () => {console.log(`server levantado en el puerto ${PORT}`)} );
