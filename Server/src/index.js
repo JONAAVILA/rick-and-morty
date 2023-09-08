d5e4d45c1eefc76a0d0dfd6dcc2d9cd1cccde24b
JONAAVILA <jona_1969@hotmail.com>
2023-09-08 11:56:42 -0300

    "server complete"

--- Server/src/index.js
+++ Server/src/index.js
@@ -29,9 +29,29 @@
 // });
 // server.listen(PORT, () => {console.log(`server levantado en el puerto ${PORT}`)} );
 
+const Routes = require("./routes");
 const express = require("express");
+
 const server = express();
 const PORT = 3001;
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+
+server.use(express.json());
+
+server.use("/rickandmorty", Routes);
 
 server.listen(PORT, ()=>{
     console.log("Server raised in" + PORT);
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,2 +1,11 @@
-const Routes = require("express");
+const express = require("express");
+const { getCharById, login, postFav, deleteFav } = require("../controllers");
 
+const Routes = express.Router();
+
+Routes.get("/character/:id",getCharById);
+Routes.get("/login", login);
+Routes.post("/fav", postFav);
+Routes.delete("/fav/:id",deleteFav);
+
+module.exports = Routes;
