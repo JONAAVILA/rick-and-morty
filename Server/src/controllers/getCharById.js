d7eaec1bfc42f37b0c631b3a9d1d6e1541649950
JONAAVILA <jona_1969@hotmail.com>
2023-09-08 11:41:33 -0300

    "server create"

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,19 +1,42 @@
 const axios = require("axios");
 
-function getCharById(res,id){
+// function getCharById(res,id){
 
-    axios(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((response)=>{
-        const { name,gender,species,origin,image,status } = response.data;
-        res.writeHead(200, {'Content-Type':'application/json'} );
-        res.end(JSON.stringify({ id,name,gender,species,origin,image,status }))
+//     axios(`https://rickandmortyapi.com/api/character/${id}`)
+//     .then((response)=>{
+//         const { name,gender,species,origin,image,status } = response.data;
+//         res.writeHead(200, {'Content-Type':'application/json'} );
+//         res.end(JSON.stringify({ id,name,gender,species,origin,image,status }))
+//     })
+//     .catch((error)=>{
+//         res.writeHead(500, {'Content-Type':'text/plain'})
+//         res.end(error.message)
+//     })
+// }
+
+// module.exports = {
+//     getCharById
+// };  
+
+const URL = "https://rickandmortyapi.com/api/character/";
+
+const getCharById = (req, res)=>{
+    const { id } = req.params;
+    
+    axios(URL+id).then((response)=>{
+        if(response.status === 200){
+            const { id, status, name, species, origin, image, gender } = response.data;
+            res.json({ id, status, name, species, origin, image, gender });
+
+        } else {
+            res.status(404).json("Not fount");
+        }
     })
     .catch((error)=>{
-        res.writeHead(500, {'Content-Type':'text/plain'})
-        res.end(error.message)
+        res.status(500).json({error: message.error});
     })
 }
 
 module.exports = {
-    getCharById
-};  
+    getCharById,
+};
