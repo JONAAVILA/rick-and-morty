9b65c668abb894c0c78d67b3ea2e233af4c85788
JONAAVILA <jona_1969@hotmail.com>
2023-09-26 12:27:13 -0300

    'restyle cards'

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -37,40 +37,59 @@ export default function Card(props) {
    return (
       
       <div className="card_character">
-         {
-         isFav ? (
-         <div className="button_fav">
-            <button onClick={handleFavorite}>❤️</button>  
-         </div>
-         ) : (
-         <div className="button_fav">
-            <button onClick={handleFavorite}>🤍</button>  
-         </div>
-         )
-         }
-         <div className="imagen">
-            <img src={props.image} alt={props.name} />
-         </div>
+         
          <div className="description" >
             <Link to={`/detail/${props.id}`}>
                <h2>{props.name}</h2>
             </Link>
-            <h4>{props.status}</h4>
-            <h4>{props.species}</h4>
-            <h4>{props.gender}</h4>
-            <h4>{props.origin}</h4>
          </div>
-         <div className="close">
+         <div className="imagen">
+            <img src={props.image} alt={props.name} />
+         </div>
+         <div className="conteiner_icons">
+            <div className="box_delete">
             {showButtonX && <button onClick={()=> props.onClose(props.id)}>
-               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                <path d="M4 7l16 0"></path>
                <path d="M10 11l0 6"></path>
                <path d="M14 11l0 6"></path>
                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
-               </svg>Delete</button>}  
+               </svg></button>}  
+            </div>
+            <div className="icon_life">
+               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-activity-heartbeat" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M3 12h4.5l1.5 -6l4 12l2 -9l1.5 3h4.5"></path>
+               </svg>
+            </div>
+            {
+         isFav ? (
+            <div className="button_fav">
+               <button onClick={handleFavorite}>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                  </svg>
+               </button>  
+            </div>
+         ) : (
+            <div className="button_fav">
+               <button onClick={handleFavorite}>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                  </svg>
+               </button>  
+            </div>
+         )
+         }
+            
          </div>
+
+         
+         
       </div>
    );
 }
