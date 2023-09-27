82d6c8f5983f0a801522c959e18fbd08de7cf9ca
JONAAVILA <jona_1969@hotmail.com>
2023-09-27 11:37:57 -0300

    'detail info complete'

--- Client/src/view/Detail/Detail.jsx
+++ Client/src/view/Detail/Detail.jsx
@@ -18,14 +18,28 @@ const Detail = ()=>{
             });
             return setCharacter({});
          }, [id]);
+
     return(
         <div className="Detail_conteiner">
             
-           <div className="box_name">
-             <h1>{character.name}</h1>
+           <div className="box_data">
+               <div className="data_detail">
+                  <div>
+                     <h1>{character.name}</h1>
+                     <h4>Status: {character.status}</h4>
+                     <h4>Gender: {character.gender}</h4>
+                     <h4>Species: {character.species}</h4>
+                     <h4>Origin: {character.origin ? character.origin.name : 'Unknown'}</h4>
+                     <h4>Location: {character.location ? character.location.name : 'Unknown'}</h4>
+                  </div>
+                  <div>
+                     <img className="img_detail" src={character.image} alt={character.name}/>
+                  </div>
+               </div>
+
            </div>
             
-            <img src={character.image} alt={character.name}/>
+            <img className="img_background" src={character.image} alt={character.name}/>
          
             
         </div>
